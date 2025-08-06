30606e5ebd193f0bc326df9e6218978d34fd9e40
LANGRI <langri@Nelsons-MacBook-Pro.local>
2025-08-06 12:51:01 +0300

    title has intergrated wee, no isues so far

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from './components/navbar/navbar';
 import Hero from './components/hero/hero';
 import Services from './components/services/services';
+import Title from './components/title/title';
 const App = () => {
   return (
     <div>
         <Navbar/>
         <Hero/>
         <div className="container">
-         {/* <Title/>*/}
+          <Title/>
           <Services/>
         </div>
     </div>
---
+++ src/components/title/title.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './title.css';
+
+const Title = () => {
+  return (
+    <div className='title'>
+      <p>Our Services</p>
+      <h2>We offer a variety of services to make your events special</h2>
+    </div>
+  )
+}
+
+export default Title
