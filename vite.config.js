44876f9203d296d4994722210ec29c379f1d9b05
LANGRI <langri@Nelsons-MacBook-Pro.local>
2025-08-07 01:28:44 +0300

    updated and working

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Kidew-Arts-website/',
+  /*base: '/Kidew-Arts-Website/',*/
   plugins: [react()],
 })
